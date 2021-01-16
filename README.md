5c316f0b5db1efd62dd388983f188f3388bfe186
DARKPHEONIXSI <73525007+DARKPHEONIXSI@users.noreply.github.com>
2021-01-17 00:07:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# angryBirdsStage1
-Stage 1 Angry Birds
+# CRUMPLED-BALLS-PART-1
+
+## Crumpled Paper is an inspiration of Paper Trash.
+
+### Output Link :  https://rajdeepjaiswal.github.io/CRUMPLED-BALLS-PART-1/
--- sketch.js
+++ sketch.js
@@ -1,57 +1,53 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine, world;
-var box1, pig1;
+var ground, gameState,engine, world,dustbin,paper;
+function setup() {
+  createCanvas(800, 400);
+  rectMode(CENTER);
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+  gameState = "start";
 
-    
-    ground = new Ground(600,height,1200,20)
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
 }
 
-function draw(){
+function draw() {
+  if (gameState === "start") {
+    background("black");
+    textSize(20);
+    fill("red");
+    text("This is small game that will teach you the importance of throwing away your trash. \n                 Press Up Arrow to Start, and Up to throw away the trash.", 50, 200)
+    if (keyCode === UP_ARROW) {
+      gameState = "play"
+    }
+  }
+  if (gameState === "play") {
+    rectMode(CENTER);
     background(0);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
+    dustbin.display();
+    paper.display();
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+  }
+}
 
-    box5.display();
-    log4.display();
-    log5.display();
 
-    bird.display();
+function keyPressed(){
+  if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 12,
+      y: -13
+    });
+  }
 }
